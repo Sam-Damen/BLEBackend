784215a3e4709b99ab9c0254f0ecad3d37bc08be
samd <samd@winter.ceit.uq.edu.au>
2014-09-17 16:31:13 +1000

    Backup flows, basic level finished

--- clear.py
+++ clear.py
@@ -9,22 +9,22 @@ db = client.samd
 usr = db.users
 MAXTIME = 3
 		
-while True:
+#while True:
 	#for every unique phone in db, get time it was entered
 	# if sysTime - phoneTime > MAXTIME -> haven't seen a beacon in MAXTIME
 	# or old majmin |= new majmin -> moved into new room
 	#clear out the corresponding database documents
 	
 #ensure time formats are the same for comparison
-	sysTime = time.mktime(time.gmtime(time.time()))
-#print('SysTime: ', sysTime)
+sysTime = time.mktime(time.gmtime(time.time()))
 
-	for doc in usr.find():
-		usrTime = doc.get('_id').generation_time
-		usrTime = time.mktime(usrTime.timetuple())
-		if((sysTime - usrTime) > MAXTIME):
-			usr.remove({"_id": doc.get('_id')})
-			print("Removed")
+
+for doc in usr.find():
+	usrTime = doc.get('_id').generation_time
+	usrTime = time.mktime(usrTime.timetuple())
+	if((sysTime - usrTime) > MAXTIME):
+		usr.remove({"_id": doc.get('_id')})
+		print("Removed")
 
 
 
